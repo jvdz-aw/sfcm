2ed1ca0694328622e4eec1eb635648cf51a06486
Jurjan van der Zee <j.vanderzee@altwym.nl>
2025-04-23 10:59:47 +0200

    Remove object name introspection in favor of explicit naming in argument checking helper functions

--- R/utils.R
+++ R/utils.R
@@ -1,19 +1,9 @@
-#' Get argument name
-#'
-#' @description This helper function is used internally by the
-#' @param arg A function argument
-#' @returns The name of the argument as a string.
-get_arg_name <- function(arg) {
-  deparse(substitute(arg))
-}
-
-
 #' Check numeric arguments
 #'
 #' @description TODO
-check_arg_num <- function(arg) {
+check_arg_num <- function(arg, arg_name) {
   if (!is.numeric(arg) || arg <= 0) {
-    stop(paste("Error: parameter", get_arg_name(arg), "must be a positive number."))
+    stop(paste("Error: parameter", arg_name, "must be a positive number."))
   }
 }
 
@@ -21,8 +11,8 @@ check_arg_num <- function(arg) {
 #' Check numeric range arguments
 #'
 #' @description TODO
-check_arg_numrange <- function(arg, arg_min, arg_max) {
+check_arg_numrange <- function(arg, arg_min, arg_max, arg_name) {
   if (!is.numeric(arg) || arg < arg_min || arg > arg_max) {
-    stop(paste("Error: parameter", get_arg_name(arg) ,"must be number between", get_arg_name(arg_min), "and", get_arg_name(arg_max), "."))
+    stop(paste("Error: parameter", arg_name,"must be number between", arg_min, "and", arg_max, "."))
   }
 }
--- tests/testthat/test_utils.R
+++ tests/testthat/test_utils.R
@@ -1,26 +1,40 @@
 library(testthat)
 
+# Test whether argument name is returned on input error
+test_that("Test whether argument name is returned on input error", {
+  arg <- "10"
+  arg_name <- "flux"
+  expect_error(check_arg_num(arg, arg_name), "Error: parameter flux must be a positive number.")
+})
+
+
 # Test numeric input check success
 test_that("Test numeric input check success", {
   arg <- 4
-  expect_no_error(check_arg_num(arg))
+  arg_name <- "valid_arg"
+  expect_no_error(check_arg_num(arg, arg_name))
 })
+
 
 # Test numeric input check failure
 test_that("Test numeric input check failure", {
   arg1 <- -1
   arg2 <- "4"
-  expect_error(check_arg_num(arg1))
-  expect_error(check_arg_num(arg2))
+  arg_name <- "invalid_arg"
+  expect_error(check_arg_num(arg1, arg_name))
+  expect_error(check_arg_num(arg2, arg_name))
 })
+
 
 # Test numeric range input check success
 test_that("Test numeric range input check success", {
   arg <- 0.5
   arg_min <- 0
   arg_max <- 1
-  expect_no_error(check_arg_numrange(arg, arg_min, arg_max))
+  arg_name <- "valid_arg"
+  expect_no_error(check_arg_numrange(arg, arg_min, arg_max, arg_name))
 })
+
 
 # Test range input check failure
 test_that("Test numeric range input check failure", {
@@ -28,6 +42,7 @@ test_that("Test numeric range input check failure", {
   arg2 <- "0.5"
   arg_min <- 0
   arg_max <- 1
+  arg_name <- "invalid_arg"
   expect_error(check_arg_numrange(arg1, arg_min, arg_max))
   expect_error(check_arg_numrange(arg2, arg_min, arg_max))
 })
