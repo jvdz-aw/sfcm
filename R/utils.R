0d1a882774a96e37957ae19f68193d48523e27bc
Jurjan van der Zee <j.vanderzee@altwym.nl>
2025-06-03 23:48:37 +0200

    Add functions for checking whether columns contain valid data

--- R/utils.R
+++ R/utils.R
@@ -20,3 +20,27 @@ check_arg_numrange <- function(arg, arg_min, arg_max, arg_name) {
     stop(paste("Error: parameter", arg_name,"must be a number between", arg_min, "and", arg_max, "."))
   }
 }
+
+
+#' Check numeric columns
+#'
+#' @description This helper function checks whether a column contains positive
+#' numeric data. Stops execution and prints the column name to the user if a
+#' column contains invalid data.
+check_col_num <- function(col_values, col_name) {
+  if (any(!is.numeric(col_values)) | any(col_values < 0)) {
+    stop(paste("Error: column", col_name, "must contain positive numbers."))
+  }
+}
+
+
+#' Check numeric range columns
+#'
+#' @description This helper function checks whether a columns contains positive
+#' numbers in the range [arg_min, arg_max]. Stops execution and prints the column
+#' name to the user if it contains invalid values.
+check_col_numrange <- function(col_values, col_min, col_max, col_name) {
+  if (any(!is.numeric(col_values)) | any(col_values < col_min) | any(col_values > col_max)) {
+    stop(paste("Error: column", col_name,"must contain numbers between", col_min, "and", col_max, "."))
+  }
+}
